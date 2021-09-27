5afbc49d96cedba49864e38403c37077a422676c
afutofu <theafuza@gmail.com>
2021-09-27 23:01:52 +0700

    Added Raleway font from Google Fonts

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -9,13 +9,12 @@ const IntroComp = styled.div`
 `;
 
 const Image = styled.div`
-  width: 200px;
-  height: 200px;
   background-image: url(${musashiAvatar});
-  background-size: contain;
+  background-size: cover;
   background-repeat: no-repeat;
-  margin-right: 30px;
-
+  width: 150px;
+  height: 150px;
+  margin-right: 25px;
   box-shadow: 0px 0px 15px 3px rgba(0, 0, 0, 0.2);
 `;
 
@@ -24,13 +23,14 @@ const TextArea = styled.div`
   flex-direction: column;
   justify-content: space-evenly;
   align-items: flex-start;
+  font-family: "Raleway", sans-serif;
 `;
 
 const Text = styled.p`
   color: black;
   margin: 0;
   padding: 0;
-  font-size: 20px;
+  font-size: 26px;
 `;
 
 const Intro = () => {
