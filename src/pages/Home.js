59498c7b0684e218a542a9e6c55ea87ea1a4962e
afutofu <theafuza@gmail.com>
2021-09-28 21:45:33 +0700

    Added background image in Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,35 @@
 import styled from "styled-components";
 
+import mountains from "../assets/mountains.jpg";
 import Intro from "../components/Intro";
 
 const HomeComp = styled.div`
+  position: relative;
   width: 100vw;
   height: 100vh;
   background-color: white;
   display: flex;
   justify-content: center;
   align-items: center;
+  background-image: url(${mountains});
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+
+const AlphaBackground = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  opacity: 0.4;
 `;
 
 const Home = () => {
   return (
     <HomeComp>
+      <AlphaBackground />
       <Intro />
     </HomeComp>
   );
